aa3eb7a9dd5c142ab8e7728de1b33037ef1ee967
LukaszSerwanski <86296472+LukaszSerwanski@users.noreply.github.com>
2022-06-09 09:50:00 +0200

    Add files via upload

---
+++ mybank.sql
@@ -0,0 +1,288 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 05, 2021 at 06:24 AM
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 5.6.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mybank`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `branch`
+--
+
+CREATE TABLE `branch` (
+  `branchId` int(11) NOT NULL,
+  `branchNo` varchar(111) NOT NULL,
+  `branchName` varchar(111) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `branch`
+--
+
+INSERT INTO `branch` (`branchId`, `branchNo`, `branchName`) VALUES
+(1, '100101', 'Rzeszow'),
+(2, '100102', 'Krakow');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedback`
+--
+
+CREATE TABLE `feedback` (
+  `feedbackId` int(11) NOT NULL,
+  `message` text NOT NULL,
+  `userId` double NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `feedback`
+--
+
+INSERT INTO `feedback` (`feedbackId`, `message`, `userId`, `date`) VALUES
+(1, 'This is testing message to admin or manager ', 1, '2022-12-15 04:20:18'),
+(3, 'This is testing message to admin or manager ', 2, '2012-12-15 07:10:58');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE `login` (
+  `id` int(11) NOT NULL,
+  `email` varchar(111) NOT NULL,
+  `password` varchar(111) NOT NULL,
+  `type` varchar(111) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`id`, `email`, `password`, `type`, `date`) VALUES
+(1, 'cashier@cashier.com', 'cashier', 'cashier', '2022-12-15 08:16:17'),
+(2, 'manager@manager.com', 'manager', 'manager', '2018-12-15 14:36:29');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notice`
+--
+
+CREATE TABLE `notice` (
+  `id` int(11) NOT NULL,
+  `userId` varchar(111) NOT NULL,
+  `notice` text NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `notice`
+--
+
+INSERT INTO `notice` (`id`, `userId`, `notice`, `date`) VALUES
+(1, '1', 'Dear Customer! <br> Our privacy policy has been changed. For more information, please go to the nearest bank.', '2011-10-04 13:11:46'),
+(6, '2', 'Dear Adam,<br>\r\Our privacy policy has been changed please visit nearest <kbd> PKO </kbd> bank for new information.', '2027-12-16 06:29:23');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `otheraccounts`
+--
+
+CREATE TABLE `otheraccounts` (
+  `id` int(11) NOT NULL,
+  `accountNo` varchar(111) NOT NULL,
+  `bankName` varchar(111) NOT NULL,
+  `holderName` varchar(111) NOT NULL,
+  `balance` varchar(111) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `otheraccounts`
+--
+
+INSERT INTO `otheraccounts` (`id`, `accountNo`, `bankName`, `holderName`, `balance`, `date`) VALUES
+(1, '12001122', 'PKO', 'Jan Adamski', '40800', '2011-12-14 11:25:07'),
+(2, '12001123', 'PKO', 'Mirek Rad', '71000', '2037-12-14 11:53:07'),
+(3, '12001124', 'PKO', 'Adam Patyk', '71000', '2027-12-14 11:45:07');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaction`
+--
+
+CREATE TABLE `transaction` (
+  `transactionId` int(11) NOT NULL,
+  `action` varchar(111) NOT NULL,
+  `credit` varchar(111) NOT NULL,
+  `debit` varchar(111) NOT NULL,
+  `balance` varchar(111) NOT NULL,
+  `beneId` varchar(111) NOT NULL,
+  `other` varchar(111) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `transaction`
+--
+
+INSERT INTO `transaction` (`transactionId`, `action`, `credit`, `debit`, `balance`, `beneId`, `other`, `userId`, `date`) VALUES
+(4, 'transfer', '', '200', '', '', '12001122', 1, '2012-12-14 12:33:40'),
+(5, 'transfer', '', '200', '', '', '10054777', 1, '2011-12-14 12:56:48'),
+(6, 'transfer', '', '333', '', '', '10054777', 1, '2019-12-14 12:57:20'),
+(7, 'transfer', '', '222', '', '', '10054777', 1, '2020-12-14 12:58:19'),
+(8, 'transfer', '', '333', '', '', '10054777', 1, '2037-12-14 13:00:23'),
+(16, 'withdraw', '', '100', '', '', '23423', 1, '2047-12-15 08:31:59'),
+(17, 'deposit', '1200', '', '', '', '234232', 1, '2011-12-15 08:32:17'),
+(18, 'transfer', '', '467', '', '', '12001122', 1, '2027-12-17 06:44:48'),
+(22, 'deposit', '1200', '', '', '', '32424', 2, '2013-12-17 06:56:29'),
+(23, 'withdraw', '', '12', '', '', '23423', 2, '2045-12-17 06:59:02'),
+(24, 'deposit', '12', '', '', '', '21321', 2, '2087-12-17 06:59:20'),
+(25, 'transfer', '', '1200', '', '', '10054777', 1, '2017-12-17 07:01:37'),
+(26, 'deposit', '600', '', '', '', '342342', 2, '2012-12-17 07:04:39'),
+(27, 'withdraw', '', '1012', '', '', '23423', 2, '2087-12-17 07:04:58');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `useraccounts`
+--
+
+CREATE TABLE `useraccounts` (
+  `id` int(11) NOT NULL,
+  `email` text NOT NULL,
+  `password` text NOT NULL,
+  `name` varchar(111) NOT NULL,
+  `balance` varchar(111) NOT NULL,
+  `pesel` varchar(111) NOT NULL,
+  `number` varchar(111) NOT NULL,
+  `city` varchar(111) NOT NULL,
+  `address` varchar(111) NOT NULL,
+  `source` varchar(111) NOT NULL,
+  `accountNo` varchar(111) NOT NULL,
+  `branch` varchar(111) NOT NULL,
+  `accountType` varchar(111) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `useraccounts`
+--
+
+INSERT INTO `useraccounts` (`id`, `email`, `password`, `name`, `balance`, `pesel`, `number`, `city`, `address`, `source`, `accountNo`, `branch`, `accountType`, `date`) VALUES
+(1, 'client@gmail.com', 'client', 'Adam Koperek', '9800', '3210375555426', '03356910260', 'Warsaw', 'Polska 12', 'Programmer', '1005469', '1', 'Current', '2017-12-14 05:50:06'),
+(2, 'client2@gmail.com', 'client2', 'Jan Koperek', '16000', '3210375555343', '03356910260', 'Warsaw', 'Polska 12', 'Programmer', '10054777', '1', 'Saving', '2017-12-14 04:50:06');
+
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `branch`
+--
+ALTER TABLE `branch`
+  ADD PRIMARY KEY (`branchId`);
+
+--
+-- Indexes for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`feedbackId`);
+
+--
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `notice`
+--
+ALTER TABLE `notice`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `otheraccounts`
+--
+ALTER TABLE `otheraccounts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `transaction`
+--
+ALTER TABLE `transaction`
+  ADD PRIMARY KEY (`transactionId`);
+
+--
+-- Indexes for table `useraccounts`
+--
+ALTER TABLE `useraccounts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `branch`
+--
+ALTER TABLE `branch`
+  MODIFY `branchId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `feedback`
+--
+ALTER TABLE `feedback`
+  MODIFY `feedbackId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `login`
+--
+ALTER TABLE `login`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `notice`
+--
+ALTER TABLE `notice`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `otheraccounts`
+--
+ALTER TABLE `otheraccounts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `transaction`
+--
+ALTER TABLE `transaction`
+  MODIFY `transactionId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+--
+-- AUTO_INCREMENT for table `useraccounts`
+--
+ALTER TABLE `useraccounts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
